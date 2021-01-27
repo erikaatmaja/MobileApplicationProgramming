4786ae6c295a989cd08913b2c9846dd5ed29c156
Erika A <erikaatmaja@users.noreply.github.com>
2021-01-27 18:19:43 +0700

    WEEK01-27299

---
+++ WEEK01_27299/app/src/main/java/com/erikaatmaja/week01_27299/MainActivity.java
@@ -0,0 +1,14 @@
+package com.erikaatmaja.week01_27299;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
